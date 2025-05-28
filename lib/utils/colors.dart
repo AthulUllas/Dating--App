f78a6744fdb71153b26e689fa9a13219279e4056
Athul <athulullas72@gmail.com>
2025-05-28 11:55:15 +0530

    snackbar util done

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Colours {
-  final Color primaryColor = Colors.orangeAccent;
+  final Color primaryColor = const Color.fromARGB(255, 234, 141, 20);
   final Color secondaryColor = const Color.fromARGB(255, 255, 101, 101);
   final Color textFieldLabelColor = Colors.grey;
+  final Color primaryTextColor = Colors.black;
 }
---
+++ lib/utils/snackbar.dart
@@ -0,0 +1,33 @@
+import 'package:flash/flash.dart';
+import 'package:flutter/material.dart';
+import 'package:matrimony/utils/colors.dart';
+
+Future<void> snackBar(String message, BuildContext context) async {
+  final colors = Colours();
+  showFlash(
+    context: context,
+    duration: Duration(seconds: 1),
+    builder: (context, controller) {
+      return FlashBar(
+        controller: controller,
+        content: Center(
+          child: Padding(
+            padding: EdgeInsets.only(top: 2),
+            child: Text(message),
+          ),
+        ),
+        contentTextStyle: TextStyle(
+          fontSize: 18,
+          color: colors.primaryTextColor,
+          fontWeight: FontWeight.bold,
+        ),
+        position: FlashPosition.top,
+        behavior: FlashBehavior.floating,
+        margin: EdgeInsets.only(left: 20, right: 20, top: 24),
+        backgroundColor: colors.secondaryColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        forwardAnimationCurve: Curves.easeInOutCubicEmphasized,
+      );
+    },
+  );
+}
