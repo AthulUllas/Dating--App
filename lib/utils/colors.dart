220283b13790aabe6444f8ecf76418a53a3311f6
Athul <athulullas72@gmail.com>
2025-05-28 11:46:23 +0530

    colors util done

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Colours {
+  final Color primaryColor = Colors.orangeAccent;
+  final Color secondaryColor = const Color.fromARGB(255, 255, 101, 101);
+  final Color textFieldLabelColor = Colors.grey;
+}
