4b776652bfac4fc5c98eb81b2a9c357e3a5c8f54
Athul <athulullas72@gmail.com>
2025-06-14 18:11:39 +0530

    Color code changed but still have to change cause it is not looking good

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Colours {
-  final Color primaryColor = const Color.fromARGB(255, 234, 141, 20);
-  final Color secondaryColor = const Color.fromARGB(255, 255, 79, 79);
+  final Color primaryColor = const Color.fromRGBO(75, 0, 130, 1);
+  final Color secondaryColor = const Color.fromRGBO(255, 215, 0, 1);
   final Color textFieldLabelColor = Colors.grey;
-  final Color primaryTextColor = Colors.black;
-  final Color secondaryTextColor = Colors.white;
+  final Color primaryTextColor = const Color.fromRGBO(28, 28, 28, 1);
+  final Color secondaryTextColor = Color.fromRGBO(253, 253, 253, 1);
   final Color splashBackgroudColor = Color.fromRGBO(251, 247, 236, 1);
   final Color scaffoldBackgroundColor = const Color.fromARGB(
     255,
